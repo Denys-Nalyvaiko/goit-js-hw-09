ffc3bdd76c0ea867e0690c6b9a40488bbfae9463
Denys-Nalyvaiko <91893103+Denys-Nalyvaiko@users.noreply.github.com>
2023-09-23 21:57:01 +0000

    Deploying to gh-pages from @ Denys-Nalyvaiko/goit-js-hw-09@3f12af0b01dfdffa4ad6e18fe4578da22d990c00 🚀

---
+++ 01-color-switcher.b4e743d5.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d=0;function o(){return`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(function(){document.body.style.backgroundColor=o(),d=setInterval((()=>{document.body.style.backgroundColor=o()}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0})),e.disabled=!0;
+//# sourceMappingURL=01-color-switcher.b4e743d5.js.map
